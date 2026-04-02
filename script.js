2ce502644251c7071d71aee2b5101a715e319a2e
mechael7 <mechaelmandausanduo7@gmail.com>
2026-04-02 23:50:45 +0700

    revisi code and desain

--- script.js
+++
@@ -1,9 +0,0 @@
-const buttonNavbar = document.getElementById("button-navbar");
-const navtiveBar = document.getElementById("native-bar");
-
-console.log(navtiveBar);
-console.log(buttonNavbar)
-
-buttonNavbar.addEventListener("click", function () {
-    navtiveBar.classList.toggle("show");
-});
