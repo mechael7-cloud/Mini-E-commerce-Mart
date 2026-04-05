cfb639a87c07ae8d8eca900a73c0137a9a492168
mechael7 <mechaelmandausanduo7@gmail.com>
2026-04-05 21:42:06 +0700

    just add style simple

---
+++ script.js
@@ -0,0 +1,18 @@
+const buttonLove = document.querySelectorAll(".image-love")
+
+
+// buttonLove.addEventListener("click", () => {
+//     buttonLove.forEach(img => {
+//         img.src = "image/love-background"
+//     })
+// })
+
+
+buttonLove.forEach(img => {
+    img.addEventListener("click", () => {
+        if (img.src.includes("love.png")) {
+            img.src = "image/love-background.png"
+        } else
+        img.src = "image/love.png"
+    })
+})
